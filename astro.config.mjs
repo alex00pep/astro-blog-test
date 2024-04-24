ec289a2da8574c2fff940f008f65451275cf5b14
alex00pep <alexmtnezf@gmail.com>
2024-04-24 01:47:20 -0500

    Full static site generation configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-// import vercel from '@astrojs/vercel/serverless';
 
 import mdx from "@astrojs/mdx";
 
@@ -8,5 +7,4 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: 'static',
-  // adapter: vercel(),
 });
