9fab20833cbe6c4ffd37a75a6e29d4b114754e6a
alex00pep <alexmtnezf@gmail.com>
2024-04-29 22:52:36 -0500

    pushing to render as hosting provider, and using a SSR methodology using Astro adapter Node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import mdx from "@astrojs/mdx";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  output: 'static',
+  output: 'hybrid',
+  adapter: node({
+    mode: "standalone"
+  })
 });
